e398a59972855d362e8baa3ecab81052a5af1f2a
Juan Farfan <darkaardvark2@gmail.com>
2012-11-11 04:04:41 -0500

    won't let me merge till I do this

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121111012400) do
+ActiveRecord::Schema.define(:version => 20121111005644) do
 
   create_table "projects", :force => true do |t|
     t.string   "title"
@@ -32,14 +32,6 @@ ActiveRecord::Schema.define(:version => 20121111012400) do
 
   add_index "roles", ["name", "resource_type", "resource_id"], :name => "index_roles_on_name_and_resource_type_and_resource_id"
   add_index "roles", ["name"], :name => "index_roles_on_name"
-
-  create_table "userprojects", :force => true do |t|
-    t.integer  "user_id"
-    t.integer  "project_id"
-    t.boolean  "expert"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
